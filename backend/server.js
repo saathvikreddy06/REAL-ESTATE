c8ecef010ca18314b90e5327311f229d6fab82a0
saathvikreddy <patlollasaathvikreddy@gmail.com>
2026-03-04 10:06:15 +0530

    deployment setup

--- backend/server.js
+++ backend/server.js
@@ -37,7 +37,11 @@ app.use("/api/properties", propertyRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
+app.use(express.static(path.join(__dirname, "dist")));
 
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "dist", "index.html"));
+});
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
